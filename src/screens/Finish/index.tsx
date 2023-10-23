df5e23e1816324d219f6e73e31e7c5ed2835cfcc
Neander de Souza <neander.faria@hotmail.com>
2023-10-23 16:04:42 -0300

    Feedback Visual - Inserindo Svg

--- src/screens/Finish/index.tsx
+++ src/screens/Finish/index.tsx
@@ -1,7 +1,8 @@
-import { Text, View } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
+import { Text, View } from 'react-native';
 
 import { Button } from '../../components/Button';
+import { Stars } from '../../components/Stars';
 
 import { styles } from './styles';
 
@@ -19,6 +20,8 @@ export function Finish() {
   return (
     <View style={styles.container}>
       <View style={styles.message}>
+        <Stars />
+
         <Text style={styles.title}>
           Parabéns!
         </Text>
