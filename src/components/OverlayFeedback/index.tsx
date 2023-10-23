1b47fd0f5b8645f95fa35639c288dc936d7d9ce2
Neander de Souza <neander.faria@hotmail.com>
2023-10-23 15:52:54 -0300

    Feedback Visual - Animando o OverlayFeedback

--- src/components/OverlayFeedback/index.tsx
+++ src/components/OverlayFeedback/index.tsx
@@ -1,6 +1,13 @@
 import { BlurMask, Canvas, Rect } from '@shopify/react-native-skia';
 import { useWindowDimensions } from 'react-native';
-import Animated from 'react-native-reanimated';
+import { useEffect } from 'react';
+import Animated, { 
+    Easing,
+    useAnimatedStyle,
+    useSharedValue, 
+    withSequence, 
+    withTiming
+  } from 'react-native-reanimated';
 import { THEME } from '../../styles/theme';
 
 const STATUS = ['transparent', THEME.COLORS.BRAND_LIGHT, THEME.COLORS.DANGER_LIGHT]
@@ -10,12 +17,27 @@ type Props = {
 }
 
 export function OverlayFeedback({ status }: Props) {
-    const { height, width } = useWindowDimensions();
+    const opactity = useSharedValue(0);
 
-    const color = STATUS[status]
+  const { height, width } = useWindowDimensions();
+
+  const styleAnimated = useAnimatedStyle(() => {
+    return {
+      opacity: opactity.value
+    }
+  })
+
+  const color = STATUS[status]
+
+  useEffect(() => {
+    opactity.value = withSequence(
+      withTiming(1, { duration: 400, easing: Easing.bounce }),
+      withTiming(0)
+    )
+  },[status])
 
     return (
-        <Animated.View style={{ width, height, position: 'absolute' }}>
+        <Animated.View style={[{ width, height, position: 'absolute' }, styleAnimated]}>
             <Canvas style={{ flex: 1 }}>
                 <Rect
                     x={0}
