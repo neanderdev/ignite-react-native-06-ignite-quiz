d8fa0ce44b55ba8eb9b3195967424ec9a407126a
Neander de Souza <neander.faria@hotmail.com>
2023-10-23 16:15:54 -0300

    Feedback Tátil e Sonoro - Lidando com voltar físico no Android

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Alert, Text, View } from 'react-native';
+import { Alert, BackHandler, Text, View } from 'react-native';
 
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { Audio } from 'expo-av';
@@ -226,6 +226,12 @@ export function Quiz() {
       handleNextQuestion();
     }
   }, [points]);
+
+  useEffect(() => {
+    const backHandler = BackHandler.addEventListener('hardwareBackPress', handleStop)
+
+    return () => backHandler.remove();
+  }, [])
 
   if (isLoading) {
     return <Loading />
