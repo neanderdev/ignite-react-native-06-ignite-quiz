9da807e24c1ae1f2a37c07fd1bfed1d715615abe
Neander de Souza <neander.faria@hotmail.com>
2023-10-23 15:29:26 -0300

    Microinterações - Executando Javascript a partir da animação

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -7,6 +7,7 @@ import Animated, {
   Easing,
   Extrapolate,
   interpolate,
+  runOnJS,
   useAnimatedScrollHandler,
   useAnimatedStyle,
   useSharedValue,
@@ -33,7 +34,8 @@ interface Params {
 
 type QuizProps = typeof QUIZ[0];
 
-const CARD_INCLINATION = 10
+const CARD_INCLINATION = 10;
+const CARD_SKIP_AREA = (-200);
 
 export function Quiz() {
   const [points, setPoints] = useState(0);
@@ -166,7 +168,11 @@ export function Quiz() {
         cardPosition.value = event.translationX
       }
     })
-    .onEnd(() => {
+    .onEnd((event) => {
+      if (event.translationX < CARD_SKIP_AREA) {
+        runOnJS(handleSkipConfirm)();
+      }
+
       cardPosition.value = withTiming(0)
     })
 
